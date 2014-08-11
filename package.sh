25ef36adc72de2fc542903f5de74b98479f1b5b3
Bill Agee <billagee@gmail.com>
2014-08-11 12:48:48 -0700

    pom, plugin desc, and helper script tweaks.

--- package.sh
+++ package.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Use JDK 1.7 install; maven tests fail when 1.8 is used.
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_67.jdk/Contents/Home
+
 cd termtv
 mvn clean
 mvn package
